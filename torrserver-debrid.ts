1e08057bb100c3651f1442fa42f59965652de1bb
sinhro <sinhro-200@mail.ru>
2025-12-11 22:04:42 +0300

    done

--- torrserver-debrid.ts
+++ torrserver-debrid.ts
@@ -1,10 +1,12 @@
 /// <reference path="./debrid-provider.d.ts" />
 /// <reference path="./core.d.ts" />
 
+declare var settings: any;  // Заглушка для this.settings (Seanime inject'ит)
+
 class TorrServerDebrid implements DebridProvider {
-    id = "torrserver-local"
-    name = "TorrServer (Local)"
-    icon = "https://i.ibb.co/0jR0Z3k/torrserver.png"
+    id = "torrserver-local";
+    name = "TorrServer (Local)";
+    icon = "https://i.ibb.co/0jR0Z3k/torrserver.png";
 
     getSettings(): DebridProviderSettings {
         return {
@@ -32,10 +34,11 @@ class TorrServerDebrid implements DebridProvider {
                     max: 80
                 }
             ]
-        }
+        };
     }
 
     async addMagnet(magnet: string, info: DebridAddMagnetInfo): Promise<DebridTorrent> {
+        const host = settings?.host || "http://127.0.0.1:8090";  // Используем глобальные settings
         const body: any = {
             action: "add",
             link: magnet,
@@ -43,37 +46,38 @@ class TorrServerDebrid implements DebridProvider {
             poster: info.poster || "",
             save: true,
             preload: true
-        }
+        };
 
-        const resp = await fetch(`${this.settings.host}/torrents`, {
+        const resp = await fetch(`${host}/torrents`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(body)
-        })
+        });
 
-        if (!resp.ok) throw new Error("TorrServer не отвечает или ошибка авторизации")
+        if (!resp.ok) throw new Error("TorrServer не отвечает или ошибка авторизации");
 
-        const json = await resp.json()
-        const hash = json.hash || json.data?.hash?.toLowerCase()
+        const json = await resp.json();
+        const hash = json.hash || json.data?.hash?.toLowerCase();
 
-        if (!hash) throw new Error("TorrServer не вернул hash")
+        if (!hash) throw new Error("TorrServer не вернул hash");
 
         return {
             id: hash,
             hash: hash,
             name: info.title || "Unknown",
             files: []
-        }
+        };
     }
 
     async getStream(torrent: DebridTorrent, file: DebridFile): Promise<DebridStream> {
-        const index = file.index ?? 0
-        const filename = encodeURIComponent(file.name || file.path || "video")
+        const host = settings?.host || "http://127.0.0.1:8090";
+        const index = file.index ?? 0;
+        const filename = encodeURIComponent(file.name || file.path || "video");
 
         return {
-            url: `${this.settings.host}/stream/${torrent.hash}/${index}/${filename}`,
-            preload: this.settings.preload || 25
-        }
+            url: `${host}/stream/${torrent.hash}/${index}/${filename}`,
+            preload: settings?.preload || 25
+        };
     }
 
     async getTorrentFiles(torrent: DebridTorrent): Promise<DebridFile[]> {
@@ -87,28 +91,29 @@ class TorrServerDebrid implements DebridProvider {
                 path: torrent.name,
                 size: 0
             }
-        ]
+        ];
     }
 
     async listTorrents(): Promise<DebridTorrent[]> {
+        const host = settings?.host || "http://127.0.0.1:8090";
         try {
-            const resp = await fetch(`${this.settings.host}/torrents`, {
+            const resp = await fetch(`${host}/torrents`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ action: "list" })
-            })
-            if (!resp.ok) return []
-            const data = await resp.json()
+            });
+            if (!resp.ok) return [];
+            const data = await resp.json();
             return (data.data || []).map((t: any) => ({
                 id: t.hash,
                 hash: t.hash.toLowerCase(),
                 name: t.title || t.hash,
                 files: []
-            }))
+            }));
         } catch {
-            return []
+            return [];
         }
     }
 }
 
-export default TorrServerDebrid
+export default TorrServerDebrid;
